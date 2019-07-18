03a3ce4ea4d522143987dd4951f331a9bef378bb
nataschaberg <natalja.wittenberg@gmx.de>
2019-07-18 21:44:07 +0200

    initialize

---
+++ App.tsx
@@ -0,0 +1,45 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * Generated with the TypeScript template
+ * https://github.com/emin93/react-native-template-typescript
+ *
+ * @format
+ */
+
+import React, {Fragment} from 'react';
+import {
+  SafeAreaView,
+  StyleSheet,
+  ScrollView,
+  View,
+  Text,
+  StatusBar,
+} from 'react-native';
+
+const App = () => {
+  return (
+    <Fragment>
+      <View style={styles.parentContainer}>
+        <View style={styles.innerContainer}>
+          <Text>Initialized</Text>
+        </View>
+      </View>
+    </Fragment>
+  );
+};
+
+const styles = StyleSheet.create({
+  parentContainer: {
+    height: '100%',
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'white'
+  },
+  innerContainer: {
+    backgroundColor: 'yellow' 
+  }
+});
+
+export default App;
